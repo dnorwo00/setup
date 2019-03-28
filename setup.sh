b0a1ba24e005e7a0ca2f022a70f31c2859e7a479
kentukynitemare <>
2019-03-28 10:07:09 -0500

    Update setup.sh, add -y to the install line and echo a message at list --upgradable.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt update -y && sudo apt install scdaemon byobu glances screenfetch
+sudo apt update -y && 
+sudo apt install scdaemon byobu glances screenfetch -y 
+echo -e "\n---\n\nThe following applications can be upgraded. Commencing in 5 seconds...\n\n---\n\n"
 apt list --upgradable
 sleep 5
 sudo apt upgrade -y
