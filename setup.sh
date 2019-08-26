cedc387ec903fae1939d874dc0e6d2d3bfecdc92
ben <bhenderson@wwlinc.com>
2019-08-26 12:43:57 -0500

    Add net-tools (netstat) to basic setup.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y && 
-sudo apt install scdaemon byobu glances screenfetch curl -y 
+sudo apt install scdaemon byobu glances screenfetch curl net-tools -y 
 echo -e "\n---\n\nThe following applications can be upgraded. Commencing in 5 seconds...\n\n---\n\n"
 apt list --upgradable
 sleep 5
