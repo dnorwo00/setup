36a8975f15ba3fc7d6418756a900831194f48d4a
ben <cbenhenderson@gmail.com>
2020-01-25 00:21:00 -0600

    Swap zerotier install script to using the saved gpg key. Appears githubusercontent link is dead.

--- vpn/zerotier-setup.sh
+++ vpn/zerotier-setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -s 'https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg' | gpg --import && \
+#curl -s 'https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg' | gpg --import && \
+gpg --import ztkey.gpg && \
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
